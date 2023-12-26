4b267b6758316e759dc1348546f8a524fe9cf843
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-12-26 16:24:36 +0400

    Radar series implementation

--- src/main/java/com/company/chartsaddon/view/series/radarseries/RadarSeriesView.java
+++ src/main/java/com/company/chartsaddon/view/series/radarseries/RadarSeriesView.java
@@ -1,22 +1,24 @@
 package com.company.chartsaddon.view.series.radarseries;
 
-
-import com.company.chartsaddon.entity.CountryLitres;
 import com.company.chartsaddon.view.main.MainView;
-
 import com.vaadin.flow.router.Route;
 import io.jmix.chartsflowui.component.Chart;
-import io.jmix.chartsflowui.data.ContainerChartItems;
+import io.jmix.chartsflowui.data.item.MapDataItem;
 import io.jmix.chartsflowui.kit.component.model.DataSet;
 import io.jmix.chartsflowui.kit.component.model.Title;
+import io.jmix.chartsflowui.kit.component.model.Tooltip;
 import io.jmix.chartsflowui.kit.component.model.axis.Radar;
 import io.jmix.chartsflowui.kit.component.model.legend.Legend;
 import io.jmix.chartsflowui.kit.component.model.series.RadarSeries;
+import io.jmix.chartsflowui.kit.data.chart.ListChartItems;
 import io.jmix.flowui.UiComponents;
-import io.jmix.flowui.data.grid.ContainerDataGridItems;
-import io.jmix.flowui.model.CollectionContainer;
-import io.jmix.flowui.view.*;
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.Subscribe;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
 
 @Route(value = "RadarSeries", layout = MainView.class)
 @ViewController("RadarSeriesView")
@@ -25,8 +27,6 @@ public class RadarSeriesView extends StandardView {
 
     @Autowired
     private UiComponents uiComponents;
-    @ViewComponent
-    private CollectionContainer<CountryLitres> countryLitresDc;
 
     @Subscribe
     protected void onInit(InitEvent event) {
@@ -44,22 +44,63 @@ public class RadarSeriesView extends StandardView {
                 new Legend()
         );
 
+        chart.setTooltip(
+                new Tooltip()
+        );
+
         chart.setRadar(
                 new Radar()
+                        .withIndicators(
+                                new Radar.Indicator()
+                                        .withName("Russia")
+                                        .withMax(300),
+                                new Radar.Indicator()
+                                        .withName("Ireland")
+                                        .withMax(300),
+                                new Radar.Indicator()
+                                        .withName("Germany")
+                                        .withMax(300),
+                                new Radar.Indicator()
+                                        .withName("Australia")
+                                        .withMax(300),
+                                new Radar.Indicator()
+                                        .withName("Austria")
+                                        .withMax(300),
+                                new Radar.Indicator()
+                                        .withName("UK")
+                                        .withMax(300),
+                                new Radar.Indicator()
+                                        .withName("Belgium")
+                                        .withMax(300)
+                        )
         );
 
         chart.setDataSet(
                 new DataSet()
                         .withSource(
-                                new DataSet.Source<>()
-                                        .withDataProvider(new ContainerChartItems(countryLitresDc))
-                                        .withCategoryField("country")
-                                        .withValueField("litres")
+                                new DataSet.Source<MapDataItem>()
+                                        .withDataProvider(
+                                                new ListChartItems<>(
+                                                        new MapDataItem(Map.of(
+                                                                "Russia", 256.9,
+                                                                "Ireland", 131.1,
+                                                                "Germany", 115.8,
+                                                                "Australia", 109.9,
+                                                                "Austria", 108.3,
+                                                                "UK", 65.0,
+                                                                "Belgium", 40.0
+                                                        ))
+                                                )
+                                        )
+                                        .withValueFields(
+                                                "Russia", "Ireland", "Germany", "Australia", "Austria", "UK", "Belgium"
+                                        )
                         )
         );
 
         chart.addSeries(
                 new RadarSeries()
+                        .withName("Litres of beer per year")
         );
 
         getContent().add(chart);
